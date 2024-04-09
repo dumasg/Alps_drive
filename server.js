e65979895a935518a49667c37c1f5ab264da96c6
DUMAS <geremydumas@gmail.com>
2024-04-09 15:33:19 +0200

    iteration 5 done

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { router } from './router.js'
+import { cors } from './middleware/cors.js'
 
 //const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(cors)
 
 app.get('/', (req, res) => {
     res.send('Hello world')
